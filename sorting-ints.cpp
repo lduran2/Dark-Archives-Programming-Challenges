d31de93c16a367efc53edfdea6c44fd907835eab
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2018-12-17 11:37:47 -0500

    sorting-ints: basic array of ints print

--- sorting-ints.cpp
+++ sorting-ints.cpp
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+void arrprt(size_t len, int* arr)
+{
+    printf("%d", *arr);
+    for (int k = 1; k < len; ++k)
+    {
+        printf(",%d", arr[k]);
+    }
+}
+
+int main(int argc, char** argv)
+{
+    const int LEN = 6;
+    int test [] = {21, 2, 32713, 41, 5, 70};
+    arrprt(LEN, test);
+
+    return 0;
+}
