5f35c45e6e23d5fbf1bd49e56c543488ff166469
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2018-12-17 13:59:09 -0500

    sorting-ints: generic with to string join

--- sorting-ints.cpp
+++ sorting-ints.cpp
@@ -1,9 +1,64 @@
+/*
+ * sorting-ints.c
+ * Sorts an array of 100 random integers.
+ * Then counts by 3s and 5s to 1000 but doesn't display duplicates.
+ * by: Leomar Durán <https://github.com/lduran2>
+ * for: https://github.com/lduran2/Dark-Archives-Programming-Challenges/blob/master/sorting-ints.cpp
+ * time: 2018-12-17 t13:57
+ */
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
 /******************************************************************//**
- * Formats an integer preceded by a separator and writes it to $str.
+ * Formats an integer preceded by a separator and writes it to @str.
+ */
+int isfmt(char* str, void** ptr, char* separator);
+
+/******************************************************************//**
+ * Formats a double preceded by a separator and writes it to @str.
+ */
+int dsfmt(char* str, void** ptr, char* separator);
+
+/******************************************************************//**
+ * Joins an array of the length specified by $len and with elements
+ * of the size specified by $element_size as a string written to @str,
+ * using the specified formating function and with the given separator.
+ */
+char* arrtos
+(
+	char* str, size_t len, void** arr, size_t element_size,
+	int (*format)(char* str, void** ptr, char* separator), char* separator
+);
+
+int main(int argc, char** argv)
+{
+	/* length of test arrays */
+	const int LEN = 6;
+	/* int test */
+	int test [] = {-32021.79, 2.2, 32713.13, 41.1, 5.5, 70.0};
+	/* double test */
+	double test2 [] = {-32021.79, 2.2, 32713.13, 41.1, 5.5, 70.0};
+
+	/* enough room for 6 six-digit ints and 6 fifteen-digit doubles */
+	char* join = (char*)(malloc(((5*2) + (6*6) + (15*6) + 1 + 100) * sizeof(char)));
+
+	/* test ints */
+	strcpy(join, "int: ");
+	arrtos(join + strlen(join), LEN, (void**)test, sizeof(*test), isfmt, ",");
+
+	/* test doubles */
+	strcat(join, "\ndbl: ");
+	arrtos(join + strlen(join), LEN, (void**)test2, sizeof(*test2), dsfmt, ";");
+
+	/* print results */
+	printf("Hello, world!\n%s\n%d\n", join, strlen(join));
+
+	return 0;
+} /* end &main(int argc, char** argv) */
+
+/******************************************************************//**
+ * Formats an integer preceded by a separator and writes it to @str.
  */
 int isfmt(char* str, void** ptr, char* separator)
 {
@@ -20,6 +75,11 @@ int dsfmt(char* str, void** ptr, char* separator)
 	return printed;
 } /* end &dsfmt(char* str, void** ptr, char* separator) */
 
+/******************************************************************//**
+ * Joins an array of the length specified by $len and with elements
+ * of the size specified by $element_size as a string written to @str,
+ * using the specified formating function and with the given separator.
+ */
 char* arrtos
 (
 	char* str, size_t len, void** arr, size_t element_size,
@@ -70,28 +130,3 @@ char* arrtos
      )
    */
 
-int main(int argc, char** argv)
-{
-	/* length of test arrays */
-	const int LEN = 6;
-	/* int test */
-	int test [] = {-32021.79, 2.2, 32713.13, 41.1, 5.5, 70.0};
-	/* double test */
-	double test2 [] = {-32021.79, 2.2, 32713.13, 41.1, 5.5, 70.0};
-
-	/* enough room for 6 six-digit ints and 6 fifteen-digit doubles */
-	char* join = (char*)(malloc(((5*2) + (6*6) + (15*6) + 1 + 100) * sizeof(char)));
-
-	/* test ints */
-	strcpy(join, "int: ");
-	arrtos(join + strlen(join), LEN, (void**)test, sizeof(*test), isfmt, ",");
-
-	/* test doubles */
-	strcat(join, "\ndbl: ");
-	arrtos(join + strlen(join), LEN, (void**)test2, sizeof(*test2), dsfmt, ";");
-
-	/* print results */
-	printf("Hello, world!\n%s\n%d\n", join, strlen(join));
-
-	return 0;
-} /* end &main(int argc, char** argv) */
