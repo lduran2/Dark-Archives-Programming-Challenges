377b97564a52a9c6ab725e04724e59131d30e1c9
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2018-12-17 11:43:20 -0500

    sorting-ints: semi-generic

--- sorting-ints.cpp
+++ sorting-ints.cpp
@@ -1,11 +1,23 @@
 #include <stdio.h>
 
-void arrprt(size_t len, int* arr)
+void arrprt(size_t len, void** arr, size_t element_size)
 {
+    /* pointer to array bytes */
+    char* ptr;
+    /* if array is empty, stop here */
+    if (len <= 0)
+    {
+        return;
+    }
+
+    ptr = ((char*)arr);
     printf("%d", *arr);
     for (int k = 1; k < len; ++k)
     {
-        printf(",%d", arr[k]);
+        /* move to next element */
+        ptr += element_size;
+        /* convert pointer before printing */
+        printf(",%d", *(int*)ptr);
     }
 }
 
@@ -13,7 +25,7 @@ int main(int argc, char** argv)
 {
     const int LEN = 6;
     int test [] = {21, 2, 32713, 41, 5, 70};
-    arrprt(LEN, test);
+    arrprt(LEN, (void**)test, sizeof(int));
 
     return 0;
 }
