4e606a705a2f719032f29d2fd6a89080525d8cf6
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2018-12-22 23:18:29 -0500

    Update benchmark-sorting-ints.cs

--- sorting-ints/benchmark-sorting-ints.cs
+++ sorting-ints/benchmark-sorting-ints.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace io
 {
@@ -16,18 +17,25 @@ namespace io
 					public const int N_TRIAL_TYPES = 2;
 					public static readonly int N_WARMUPS = N_TRIALS[0];
 					public static readonly int N_REPEATS = N_TRIALS[1];
-					public const long TIME = 25_955; // moderate 10s of seconds
+					// public const long TIME = 25_955; // moderate 10s of seconds
 
 					public static void Main(string[] args)
 					{
 						long[,] timings;
 						int array_size;
+
+						timings = new long[N_WARMUPS, N_LOOPS];
+
+						/* parse the arguments */
+						/* if no size is given, default to 100 */
 						if (args.Length == 0) args = new string[1]{"100"};
-						timings = new long[N_WARMUPS,N_LOOPS];
+						/* if another bad value is given, stop */
 						if (!Int32.TryParse(args[0], out array_size))
 						{
 							return;
 						}
+
+						/* first do a warmup round, then the actual round */
 						for (int i_trial_type = 0; (i_trial_type < N_TRIAL_TYPES); i_trial_type++)
 						{
 							for (int i_round = N_TRIALS[i_trial_type]; (i_round-- > 0); )
@@ -39,52 +47,141 @@ namespace io
 								int n_counts = (array_size + 1);
 								Random randoms;
 
+								/* cleanup before each round */
 								GC.Collect();
 								randoms = new Random();
 								array = new int[array_size];
 								counts = new int[n_counts];
 
+								/* sort $N_LOOPS times */
 								for (int i_loop = N_LOOPS; (i_loop-- > 0); )
 								{
+									/* prepare random array */
 									FillRandom(array_size, array, randoms);
+									/* begin timing */
 									start = Stopwatch.GetTimestamp();
+									/* cleanup counts array */
 									Fill(0, n_counts, counts, 0);
+									/* sort the random array */
 									CountSort(array_size, array, n_counts, counts);
+									/* end timing */
 									finish = Stopwatch.GetTimestamp();
 									timings[i_round, i_loop] = (finish - start);
 								}
 
+								/* print the last element of the last array to make sure the
+								   VM sorts every array completely */
 								Console.Error.Write("{0} ", array[array_size-1]);
 							}
 						}
 						Console.Error.WriteLine();
-						for (int i_round = N_REPEATS; (i_round-- > 0); )
+
+						/* print the summaries */
+						for (int i_round = 1; (i_round <= N_REPEATS); i_round++)
 						{
-							double mean;
-							double time_sum = 0;
+							/* mean related */
+							double ari_mean;
+							double ari_time_sum = 0;
+							int left;
+							int right;
+							bool isInlier = true;
+							double har_mean;
+							double har_time_sum = 0;
+							/* minimum and maximum */
 							double min;
 							double max;
-							long min_ticks = Int64.MaxValue;
-							long max_ticks = Int64.MinValue;
+							long min_ticks;
+							long max_ticks;
+
+							/* quartile related */
+							double inlier_min;
+							double inlier_max;
+							double Q1;
+							double Q2;
+							double Q3;
+							long[] round_timings = new long[N_LOOPS];
+							double IQR;
+							double Q1_ticks;
+							double Q2_ticks;
+							double Q3_ticks;
+							int iQ1;
+							int iQ2;
+
+							/* copy the timings before starting */
+							Buffer.BlockCopy(timings, i_round * N_LOOPS * Marshal.SizeOf<long>(), round_timings, 0, N_LOOPS * Marshal.SizeOf<long>());
+							/* sort the timings to help find the min, max and quartiles */
+							Array.Sort(round_timings);
+
+							/* find the harmonic mean */
 							for (int i_loop = N_LOOPS; (i_loop-- > 0); )
 							{
-								time_sum += ((double)timings[i_round, i_loop]);
-								if (timings[i_round, i_loop] < min_ticks)
+								har_time_sum += 1.0/((double)round_timings[i_loop]);
+							}
+							har_mean = (N_LOOPS / har_time_sum);
+
+							/* find the minimum and maximum */
+							min_ticks = round_timings[0];
+							max_ticks = round_timings[N_LOOPS - 1];
+
+							/* calculate indices for the mean and first quartile */
+							iQ2 = ((int)Math.Ceiling(((double)N_LOOPS)/2.0));
+							iQ1 = ((int)Math.Ceiling(((double)iQ2)/2.0));
+
+							/* calculate the actual quartiles */
+							Q1_ticks = (round_timings[iQ1] + round_timings[((int)Math.Floor(iQ2/2.0)) + 1])/2.0;
+							Q2_ticks = (round_timings[iQ2] + round_timings[((int)Math.Floor(N_LOOPS/2.0)) + 1])/2.0;
+							Q3_ticks = (round_timings[iQ1 + iQ2] + round_timings[(((int)Math.Floor(iQ2/2.0)) + 1)] + iQ2)/2.0;
+
+							/* find the range for outliers, what's not in [inlier_min, inlier_max] */
+							IQR = (Q3_ticks - Q1_ticks);
+							inlier_min = (Q1_ticks - 1.5 * IQR);
+							inlier_max = (Q3_ticks + 1.5 * IQR);
+
+							/* find the edge to the lesser outliers, adding all timings until then */
+							for (left = (iQ2 + 1); (left-- > 0) && isInlier; )
+							{
+								if (round_timings[left] < inlier_min)
 								{
-									// Console.WriteLine(">>[min]> {0}>{1}", min_ticks, timings[i_round, i_loop]);
-									min_ticks = timings[i_round, i_loop];
+									isInlier = false;
 								}
-								if (timings[i_round, i_loop] > max_ticks)
-								{
-									// Console.WriteLine(">>[max]> {0}<{1}", max_ticks, timings[i_round, i_loop]);
-									max_ticks = timings[i_round, i_loop];
+								else {
+									ari_time_sum += round_timings[left];
 								}
 							}
-							mean = ((((time_sum / N_LOOPS) * 1.0e+6)) / Stopwatch.Frequency);
+
+							/* find the edge to the greater outliers, likewise */
+							isInlier = true;
+							for (right = (iQ2 + 1); (right < N_LOOPS) && isInlier; ++right)
+							{
+								if (round_timings[right] > inlier_max)
+								{
+									isInlier = false;
+								}
+								else {
+									ari_time_sum += round_timings[right];
+								}
+							}
+
+							/* replace all outliers with the harmonic mean */
+							ari_time_sum += (N_LOOPS - (right - left + 1)) * har_mean;
+							/* finish calculating the arithmetic mean */
+							ari_mean = ((((ari_time_sum / N_LOOPS) * 1.0e+6)) / Stopwatch.Frequency);
+
+							/* finish converting the quartiles */
 							min = ((min_ticks * 1.0e+6) / Stopwatch.Frequency);
+							Q1 = ((Q1_ticks * 1.0e+6) / Stopwatch.Frequency);
+							Q2 = ((Q2_ticks * 1.0e+6) / Stopwatch.Frequency);
+							Q3 = ((Q3_ticks * 1.0e+6) / Stopwatch.Frequency);
 							max = ((max_ticks * 1.0e+6) / Stopwatch.Frequency);
+
+							/* print the results */
+							Console.WriteLine("Round {0}", i_round);
+							// Console.WriteLine("[{0}]", string.Join(", ", round_timings));
 							Console.WriteLine("min : {0}", min);
-							Console.WriteLine("mean: {0}", mean);
+							Console.WriteLine("Q1  : {0}", Q1);
+							Console.WriteLine("Q2  : {0}", Q2);
+							Console.WriteLine("mean: {0}", ari_mean);
+							Console.WriteLine("Q3  : {0}", Q3);
 							Console.WriteLine("max : {0}", max);
 							Console.WriteLine();
 						}
